232d974e06065030a939cd04f23406c664520685
Septian A Tama <sfirst.tama@gmail.com>
2017-05-16 21:03:28 +0700

    retrieve data from firebase ok

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -10,47 +10,57 @@
 
     Row(v-bind:gutter="16")
       Col(:xs="24", :sm="24", :md="6", :lg="6")
-        BoardSection(title="Un-Assigned Task", :tasks="tasks")
+        BoardSection(title="Un-Assigned Task", :tasks="unassigned")
       Col(:xs="24", :sm="24", :md="6", :lg="6")
-        BoardSection(title="To-Do", :tasks="tasks")
+        BoardSection(title="To-Do", :tasks="todo")
       Col(:xs="24", :sm="24", :md="6", :lg="6")
-        BoardSection(title="Doing", :tasks="tasks")
+        BoardSection(title="Doing", :tasks="doing")
       Col(:xs="24", :sm="24", :md="6", :lg="6")
-        BoardSection(title="Done", :tasks="tasks")
-
+        BoardSection(title="Done", :tasks="done")
 </template>
 
 <script>
 import BoardSection from './BoardSection'
 import FormNewTask from './FormNewTask'
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyDOdqkhCFuTjkA6evws6sTZfVq1RVDSygk',
+  authDomain: 'kamvan-6aa69.firebaseapp.com',
+  databaseURL: 'https://kamvan-6aa69.firebaseio.com',
+  projectId: 'kamvan-6aa69',
+  storageBucket: 'kamvan-6aa69.appspot.com',
+  messagingSenderId: '1033001407327'
+}
+
+const firebaseApp = firebase.initializeApp(config)
+const db = firebaseApp.database()
+const tasksRef = db.ref('tasks')
 export default {
   components: {
     BoardSection,
     FormNewTask
   },
+  firebase: {
+    tasks: tasksRef
+  },
   data () {
     return {
-      newTaskModal: false,
-      tasks: [
-        {
-          title: 'Task title 1',
-          desc: 'Task Description 1',
-          point: 10,
-          assignedTo: 'someone'
-        },
-        {
-          title: 'Task title 2',
-          desc: 'Task Description 2',
-          point: 10,
-          assignedTo: 'someone'
-        },
-        {
-          title: 'Task title 2',
-          desc: 'Task Description 2',
-          point: 10,
-          assignedTo: 'someone'
-        }
-      ]
+      newTaskModal: false
+    }
+  },
+  computed: {
+    unassigned () {
+      return this.tasks.filter((task) => task.status === 0)
+    },
+    todo () {
+      return this.tasks.filter((task) => task.status === 1)
+    },
+    doing () {
+      return this.tasks.filter((task) => task.status === 2)
+    },
+    done () {
+      return this.tasks.filter((task) => task.status === 3)
     }
   }
 }
--- src/components/BoardSection.vue
+++ src/components/BoardSection.vue
@@ -1,15 +1,18 @@
 <template lang="jade">
 Card(:class="classSection")
   p(slot="title") {{ title }}
-  BoardCard(v-for="(task, index) in tasks", :task="task", :key="index")
+  draggable(class="dragArea", v-model="tasks", :options="{group:'taskList'}")
+    BoardCard(v-for="(task, index) in tasks", :task="task", :key="index")
 </template>
 
 <script>
 import BoardCard from './BoardCard'
+import draggable from 'vuedraggable'
 export default {
   props: ['title', 'tasks'],
   components: {
-    BoardCard
+    BoardCard,
+    draggable
   },
   computed: {
     classSection () {
@@ -25,26 +28,33 @@ export default {
         default:
           return 'unassigned'
       }
+    },
+    tasksSection () {
+      return this.tasks
     }
   }
 }
 </script>
 
 <style lang="stylus">
+.ivu-card-head
+  border-radius: 4px 4px 0px 0px
 .unassigned > .ivu-card-head
-    background-color: #ff3300
-    p
-      color: #fff
+  background-color: #ff3300
+  p
+    color: #fff
 .to-do > .ivu-card-head
-    background-color: #ff9900
-    p
-      color: #fff
+  background-color: #ff9900
+  p
+    color: #fff
 .doing > .ivu-card-head
-    background-color: #3399ff
-    p
-      color: #fff
+  background-color: #3399ff
+  p
+    color: #fff
 .done > .ivu-card-head
-    background-color: #00cc66
-    p
-      color: #fff
+  background-color: #00cc66
+  p
+    color: #fff
+.dragArea
+  min-height: 16px;
 </style>
