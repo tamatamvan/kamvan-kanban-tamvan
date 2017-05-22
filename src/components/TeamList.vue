cdd871ed2b9fd92f8c13982d0d5b94776cb8184a
Septian A Tama <sfirst.tama@gmail.com>
2017-05-22 14:58:05 +0700

    initiate teamlist component

---
+++ src/components/TeamList.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+  #team-list
+    Row(type="flex" justify="space-between")
+      Col
+        h1 KamVan Board - Team List
+      Col
+        Button(type="primary") New Team
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Auth from '@/components/Auth'
 import Board from '@/components/Board'
+import TeamList from '@/components/TeamList'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/auth',
       name: 'Auth',
       component: Auth
+    },
+    {
+      path: '/team-list',
+      name: 'TeamList',
+      component: TeamList
     }
   ]
 })
